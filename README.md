7b3dbf2d86e0182dcfc822908e3a99dd14155955
Ismail Chaida <ismail.chaida@hellofresh.com>
2021-08-28 15:58:55 +0200

    Blog creation
    Initial commit, blog creation using Next.js

---
+++ README.md
@@ -0,0 +1,7 @@
+# Personal Blog
+
+A Next.js blog powered by [MDX](https://mdxjs.com).
+
+## Docs
+
+Visit [https://reflexjs.org/docs](https://reflexjs.org/docs) to learn more about Nextjs and Reflexjs.
---
+++ src/config.ts
@@ -0,0 +1,30 @@
+export default {
+  site: {
+    branding: {
+      name: "Blog.",
+      description: "A Personal blog built using MDX and Reflexjs.",
+      icon: "logo",
+      image: "/images/placeholder.jpg",
+    },
+    twitter: "@arshadcn",
+    copyright: `Ismail Chaida © ${new Date().getFullYear()} Blog.`,
+    links: [
+      {
+        title: "Home",
+        href: "/",
+      },
+      {
+        title: "About",
+        href: "/about",
+      },
+      {
+        title: "Styleguide",
+        href: "/blog/styleguide",
+      },
+      {
+        title: "Contact",
+        href: "/contact",
+      },
+    ],
+  },
+}
